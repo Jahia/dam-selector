8f226541e44c343fd280c34afc073573bc73484c
Hervé Duchesne <hduchesne@jahia.com>
2023-03-14 17:59:11 +0100

    first commit

---
+++ src/javascript/DamSelector/DamSelector.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+//Create a dropdown list with by default "jahia", then get from the config the list of DAM to enable <name><selectorType>
+export const CloudinaryPicker = ({field, id, value, editorContext, inputContext, onChange}) => {
+    return (
+        <></>
+
+    )
+}
---
+++ src/javascript/init.js
@@ -0,0 +1,12 @@
+import {registry} from '@jahia/ui-extender';
+import {DamSelector} from './DamSelector';
+
+export default function () {
+    registry.add('callback', 'cloudinaryPickerSelectorType',{
+        targets:['jahiaApp-init:20'],
+        callback: () => {
+            registry.add('selectorType','DamSelector', {cmp: DamSelector, supportMultiple:false});
+            console.debug('%c DamSelector Editor Extensions  is activated', 'color: #3c8cba');
+        }
+    })
+}
