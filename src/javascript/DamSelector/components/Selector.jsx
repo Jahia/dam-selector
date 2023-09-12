206273b5b94821d94989a8138e94c606b7f527e5
hduchesne <hduchesne@jahia.com>
2023-09-12 17:02:53 +0200

    review action, code cleaning, bug fix on historic

--- src/javascript/DamSelector/components/Selector.jsx
+++ src/javascript/DamSelector/components/Selector.jsx
@@ -18,15 +18,28 @@ const styles = () => ({
 
 // Create a dropdown list with by default "jahia", then get from the config the list of DAM to enable <name><selectorType>
 const SelectorCmp = ({classes, ...props}) => {
-    const {damSelectorConfigs, valueChoiceListConfig, field, id, inputContext, value, onChange} = props;
+    const {damSelectorConfigs, valueChoiceListConfig, field, id, inputContext, value,onChange,onBlur} = props;
     const {t} = useTranslation();
 
-    const [selectedChoiceListConfig, setSelectedChoiceListConfig] = React.useState(valueChoiceListConfig);
+    const [managedValue,setManagedValue] = React.useState({config:valueChoiceListConfig,value});
+
+    // const [historicalChoiceListValue, setHistoricalChoiceListValue] = React.useState((valueChoiceListConfig && valueChoiceListConfig.key) ? {[valueChoiceListConfig.key]:value} : {})
+    const [historicalChoiceListValue, setHistoricalChoiceListValue] = React.useState({})
+
+    React.useEffect(() => {
+        setManagedValue({config:valueChoiceListConfig,value})
+    },[value,valueChoiceListConfig]);
+
+    React.useEffect(() => {
+        //clear historic
+        if(!value)
+            setHistoricalChoiceListValue({})
+    },[value]);
 
     const {readOnly, label, iconName, dropdownData} = React.useMemo(() => ({
         readOnly: field.readOnly /* || field.valueConstraints.length === 0 */,
-        label: t(selectedChoiceListConfig?.label || 'dam-selector:label.dropDown.emptyLabel'),
-        iconName: selectedChoiceListConfig?.icon || '',
+        label: t(managedValue.config?.label || 'dam-selector:label.dropDown.emptyLabel'),
+        iconName: managedValue.config?.icon || '',
         dropdownData: damSelectorConfigs.length > 0 ? damSelectorConfigs.map(({key: picker, label, icon, description}) => {
             // Const image = item.properties?.find(property => property.name === 'image')?.value;
             // const description = item.properties?.find(property => property.name === 'description')?.value;
@@ -44,7 +57,21 @@ const SelectorCmp = ({classes, ...props}) => {
                 }
             };
         }) : [{label: '', value: ''}]
-    }), [t, field, selectedChoiceListConfig, damSelectorConfigs]);
+    }), [t, field, managedValue, damSelectorConfigs]);
+
+    const handleChange = (item) => {
+        if (item.value !== managedValue.config?.key) {
+            const changedChoiceListConfig = damSelectorConfigs.find(({key: picker}) => picker === item.value);
+            const histoEntry = managedValue.config && managedValue.config.key ? {[managedValue.config.key] : managedValue.value} : {};
+
+            setManagedValue({
+                config:changedChoiceListConfig,
+                value: historicalChoiceListValue[changedChoiceListConfig.key] || null
+            });
+
+            setHistoricalChoiceListValue({...historicalChoiceListValue,...histoEntry});
+        }
+    }
 
     return (
         <>
@@ -60,35 +87,34 @@ const SelectorCmp = ({classes, ...props}) => {
                     size="medium"
                     data={dropdownData}
                     label={label}
-                    value={selectedChoiceListConfig?.key}
+                    value={managedValue.config?.key}
                     icon={iconName && toIconComponent(iconName)}
                     hasSearch={dropdownData && dropdownData.length >= 5}
                     searchEmptyText={t('content-editor:label.contentEditor.global.noResult')}
-                    onChange={(evt, item) => {
-                        if (item.value !== selectedChoiceListConfig?.key) {
-                            const changedChoiceListConfig = damSelectorConfigs.find(({key: picker}) => picker === item.value);
-
-                            if (changedChoiceListConfig.value) {
-                                onChange(changedChoiceListConfig.value);
-                            }
-                                setSelectedChoiceListConfig(changedChoiceListConfig);
-                        }
-                    }}
-                    // OnBlur={onBlur}
+                    onChange={(evt, item) => handleChange(item)}
+                    onBlur={onBlur}
                 />
                 {inputContext.displayActions && value && (
-                <DisplayAction actionKey="content-editor/field/DamSelector"
-                               value={value}
-                               field={field}
-                               inputContext={inputContext}
-                               render={ButtonRenderer}
-                        />
+                <DisplayAction {...{
+                    actionKey:"content-editor/field/DamSelector",
+                    value,
+                    field,
+                    inputContext:{
+                        ...inputContext,
+                        actionContext : {
+                            onChange,
+                            onBlur
+                        }
+                    },
+                    render:ButtonRenderer
+                }} />
                 )}
             </div>
             <div className="flexFluid flexRow alignCenter">
                 <PickerComponent {...{
                     ...props,
-                    choiceListConfig: selectedChoiceListConfig,
+                    choiceListConfig: managedValue.config,
+                    value: managedValue.value,
                     inputContext: {
                         ...inputContext,
                         displayActions : false
@@ -103,11 +129,12 @@ SelectorCmp.propTypes = {
     classes: PropTypes.object.isRequired,
     field: FieldPropTypes.isRequired,
     id: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
     inputContext: PropTypes.object.isRequired,
     damSelectorConfigs: PropTypes.array.isRequired,
-    valueChoiceListConfig: PropTypes.array.isRequired
+    valueChoiceListConfig: PropTypes.object.isRequired
 };
 
 export const Selector = withStyles(styles)(SelectorCmp);
